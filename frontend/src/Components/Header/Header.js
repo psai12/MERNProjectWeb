51d85f39419956a19adc07006de0f02f593b92d8
psai12 <sairampallapothula@gmail.com>
2024-03-06 00:17:12 +0530

    Pges creation

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -6,7 +6,16 @@ function Header()
 return(
     <div className='HeaderContainer'>
       <div className='NavContainer'>
-        <h6>MENU</h6>
+        <h6 className='MenuHeader'>MENU
+        <div className='SubMenuParent'>
+             <div className='SubMenuChild'>
+               <h6 onClick={()=>{window.location.href="/Cones"}}>CONES</h6>
+               <h6 onClick={()=>{window.location.href="/Pines"}}>PINTS</h6>
+               <h6 onClick={()=>{window.location.href="/Cakes"}}>CAKES</h6>
+               <h6 onClick={()=>{window.location.href="/Cheesecakes"}}>CHEESECAKES</h6>
+               </div>
+        </div>
+        </h6>
         <h6>LOCATIONS</h6>
         <h6>CATERING</h6>
         <img src={ice} width='100' height='100'/>
