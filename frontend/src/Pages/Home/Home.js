7dc1608c196b1057b06c7d93bc8632bc04bfdc6b
psai12 <sairampallapothula@gmail.com>
2024-03-05 20:36:32 +0530

    Headercreation

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,10 +1,17 @@
 import { Header } from "../../Components/Header/Header";
+import texture from '../../Assets/texture.jpg';
+import cones from '../../Assets/Cones.png';
 
-
+import './Home.css';
 function Home()
 {
-    return <div>
+    return <div className="HomeParent">
       <Header/>
+       <div className="Menu">
+        <img src={texture} width="100%" height="100%"/>
+        <img src={cones} width="800" height="200" className="logo"/>
+
+       </div>
     </div>
 }
 
