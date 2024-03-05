c04a7744245e91510b625928e29afa88d2e6f53b
psai12 <sairampallapothula@gmail.com>
2024-03-05 20:24:07 +0530

    Header created

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,10 +1,18 @@
 import './Header.css';
+import ice from '../../Assets/icecream.png';
 
 function Header()
 {
 return(
-    <div>
-      <p>Hello</p>
+    <div className='HeaderContainer'>
+      <div className='NavContainer'>
+        <h6>MENU</h6>
+        <h6>LOCATIONS</h6>
+        <h6>CATERING</h6>
+        <img src={ice} width='100' height='100'/>
+        <h6>SOCIAL</h6>
+        <h6>FRANCHISING</h6>
+      </div>
     </div>);
 }
 
