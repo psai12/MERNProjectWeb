087ac884dafcb3edf7debfd87265a6580ffc8c1e
psai12 <sairampallapothula@gmail.com>
2024-03-05 19:22:33 +0530

    Routes defined

---
+++ frontend/src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header()
+{
+return(
+    <div>
+      <p>Hello</p>
+    </div>);
+}
+
+export {Header};
---
+++ frontend/src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import { Header } from "../../Components/Header/Header";
+
+
+function Home()
+{
+    return <div>
+      <Header/>
+    </div>
+}
+
+export {Home};
